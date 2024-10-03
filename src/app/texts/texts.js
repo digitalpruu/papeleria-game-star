6104893c06c5e71ea3c745cef142a13d364221b7
kevinpicon01 <digitalpruu@gmail.com>
2024-10-03 18:29:52 -0500

    Fix

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,36 +1,38 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "PAPELERÍA GAME STAR";
+const slogan = "Lo que necesitas, al precio que buscas y con el servicio que mereces.";
+const email = "papeleriagamestar@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    `¡Hola! Estoy interesado en los productos de ${title} y me gustaría obtener más información. ¡Gracias!`
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "3232368979";
+
 const textos = {
     index: {
-        title: title,
+        title: `${title}`,
+        slogan: `${slogan}`,
     },
     homeContent: {
-        title: title,
+        title: `${title}`,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "En PAPELERÍA GAME STAR, ofrecemos una amplia gama de productos de papelería y miscelánea que se adaptan a tus necesidades cotidianas. Ya sea que estés buscando materiales para la escuela, la oficina o simplemente para el hogar, en PAPELERÍA GAME STAR encontrarás lo que necesitas al precio que buscas y con el servicio que mereces. Nos esforzamos por ofrecer productos de alta calidad y una experiencia de compra excepcional para cada cliente."
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
-        email: email
+        email: email,
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "",
+            p2: slogan,
         },
-        address: "Dirección del emprendimiento",
+        address: "",
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
-        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
+        facebook: "https://www.facebook.com",
+        instagram: "https://www.instagram.com",
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
     },
-
 };
+
 export default textos;
